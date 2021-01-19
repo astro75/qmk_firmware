f3b66c30003b56b05b54ff5b237d95fbdc6395a7
L. K. Post <lkpost@scept.re>
2021-01-19 22:37:29 +0100

    TB frfsf

--- keyboards/kyria/keymaps/crote/rules.mk
+++ keyboards/kyria/keymaps/crote/rules.mk
@@ -4,8 +4,8 @@ OLED_DRIVER_ENABLE = no
 # Builtin features
 CONSOLE_ENABLE = yes
 
-# Custom features
 TRACKBALL_ENABLE = yes
+POINTING_DEVICE_ENABLE = yes
 
 ifeq ($(strip $(ENCODER_ENABLE)), yes)
 	SRC += enc.c
--- users/crote/rules.mk
+++
@@ -1,4 +0,0 @@
-ifeq ($(strip $(TRACKBALL_ENABLE_OLD)), yes)
-	OPT_DEFS += -DTRACKBALL_ENABLE_OLD
-	SRC += trackball.c timer_hires.c
-endif
