317f3315b5865ac8b0e5f50fac257322cf4ebd3e
Evaldas Ciakas <evaldas757@gmail.com>
2023-05-06 00:46:07 +0300

    Enable LED underglow

--- keyboards/splitkb/kyria/keymaps/astro75/rules.mk
+++ keyboards/splitkb/kyria/keymaps/astro75/rules.mk
@@ -1,9 +1,21 @@
 OLED_ENABLE = yes
 OLED_DRIVER = SSD1306   # Enables the use of OLED displays
 ENCODER_ENABLE = no       # Enables the use of one or more encoders
-RGBLIGHT_ENABLE = no      # Enable keyboard RGB underglow
+RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
 LEADER_ENABLE = no        # Enable the Leader Key feature
 MOUSEKEY_ENABLE = no
 
 POINTING_DEVICE_ENABLE = yes
 POINTING_DEVICE_DRIVER = pimoroni_trackball
+
+
+CONSOLE_ENABLE = no
+COMMAND_ENABLE = no
+MOUSEKEY_ENABLE = no
+EXTRAKEY_ENABLE = no
+
+SPACE_CADET_ENABLE = no
+GRAVE_ESC_ENABLE = no 
+MAGIC_ENABLE = no
+
+MUSIC_ENABLE = no
